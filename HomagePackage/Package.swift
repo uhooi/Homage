6952357c9625b99527f3828b9b3769a66bfedbea
uhooi <the_uhooi@yahoo.co.jp>
2023-10-04 00:31:00 +0900

    Add HomageFeature

--- HomagePackage/Package.swift
+++ HomagePackage/Package.swift
@@ -2,6 +2,27 @@
 
 import PackageDescription
 
+let debugOtherSwiftFlags = [
+    "-Xfrontend", "-warn-long-expression-type-checking=500",
+    "-Xfrontend", "-warn-long-function-bodies=500",
+    "-strict-concurrency=complete",
+    "-enable-actor-data-race-checks",
+]
+
+let debugSwiftSettings: [PackageDescription.SwiftSetting] = [
+    .unsafeFlags(debugOtherSwiftFlags, .when(configuration: .debug)),
+    .enableUpcomingFeature("ConciseMagicFile", .when(configuration: .debug)), // SE-0274
+    .enableUpcomingFeature("ForwardTrailingClosures", .when(configuration: .debug)), // SE-0286
+    .enableUpcomingFeature("ExistentialAny", .when(configuration: .debug)), // SE-0335
+    .enableUpcomingFeature("BareSlashRegexLiterals", .when(configuration: .debug)), // SE-0354
+    .enableUpcomingFeature("ImportObjcForwardDeclarations", .when(configuration: .debug)), // SE-0384
+    .enableUpcomingFeature("DisableOutwardActorInference", .when(configuration: .debug)), // SE-0401
+]
+
+let productionFeatures: [PackageDescription.Target.Dependency] = [
+    "HomageFeature",
+]
+
 let package = Package(
     name: "HomagePackage",
     defaultLocalization: "ja",
@@ -18,7 +39,14 @@ let package = Package(
 
         .target(
             name: "ProductionApp",
-            dependencies: [],
+            dependencies: productionFeatures,
             path: "./Sources/Apps/Production"),
+
+        // MARK: Feature layer
+
+        .target(
+            name: "HomageFeature",
+            dependencies: [],
+            path: "./Sources/Features/Homage"),
     ]
 )
