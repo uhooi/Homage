5bf138a01891131211e98157db8feeaab0cd20dd
uhooi <the_uhooi@yahoo.co.jp>
2023-10-03 23:11:00 +0900

    Create project

---
+++ Homage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homage
+//
+//  Created by uhooi on 2023/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homage/HomageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomageApp.swift
+//  Homage
+//
+//  Created by uhooi on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct HomageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
